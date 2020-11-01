43e0d7489e5f8f6de4a2e7901efa718a4f50e215
happy-kernel-6078 <wolf_vii@yahoo.com>
2020-11-01 01:47:17 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,14 +1,11 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-    if index.between?(0,9)
+    #if index.between?(0,9)
       if position_taken? == false
         return true
       else
         return false
       end
-      else
-        return false
-    end
 end
 
 
